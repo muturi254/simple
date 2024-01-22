85b6b8e4700b985f8d24e4353ac6d7fe5135fe63
muturi254 <petermuturi74@gmail.com>
2024-01-22 22:16:20 +0300

    add static files configuration for bootsrap

--- simple/settings.py
+++ simple/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'boards.apps.BoardsConfig'
+    'boards.apps.BoardsConfig',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -120,7 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
