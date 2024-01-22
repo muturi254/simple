151fc0eb93556b7de7555e2f99a3ba2ca0cb917d
muturi254 <petermuturi74@gmail.com>
2024-01-22 21:23:07 +0300

    add templates for views

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from boards.models import Board
+
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, world!!")
+    boards = Board.objects.all()
+   
+    return render(request, 'home.html', {'boards': boards})
