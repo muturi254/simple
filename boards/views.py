62f76b6c3c57ad64fd862a0feede88e067bd9e3a
muturi254 <petermuturi74@gmail.com>
2024-01-23 19:35:35 +0300

    add new topics view with test for the view

--- boards/views.py
+++ boards/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from boards.models import Board
 
 # Create your views here.
 def home(request):
     boards = Board.objects.all()
-   
     return render(request, 'home.html', {'boards': boards})
+
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    # print(dir(request.body))
+    return render(request, 'topics.html', {'board': board})
--- simple/urls.py
+++ simple/urls.py
@@ -21,5 +21,6 @@ from boards import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('admin/', admin.site.urls),
 ]
